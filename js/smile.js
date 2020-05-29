bfe3caeaa7ef645517cc5ea56118450e53537bb0
Nisha2018 <38083071+Nisha2018@users.noreply.github.com>
2020-05-29 16:55:26 +0530

    changes

--- js/smile.js
+++ js/smile.js
@@ -20,7 +20,7 @@
 const slides = document.querySelectorAll('.slide');
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
-const auto = false; // Auto scroll
+const auto = true; // Auto scroll
 const intervalTime = 5000;
 let slideInterval;
 
@@ -78,3 +78,31 @@ if (auto) {
   // Run next slide at interval time
   slideInterval = setInterval(nextSlide, intervalTime);
 }
+
+
+// text animation
+
+// Wrap every letter in a span
+var textWrapper = document.querySelectorAll('.ml2');
+textWrapper[0].innerHTML = textWrapper[0].textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+textWrapper[1].innerHTML = textWrapper[1].textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+textWrapper[2].innerHTML = textWrapper[1].textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+textWrapper[3].innerHTML = textWrapper[1].textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+textWrapper[4].innerHTML = textWrapper[1].textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
